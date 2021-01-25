df70b1d5614dcec774a81dd2454094d175854eb1
joelworsham <joelworsham@gmail.com>
2021-01-25 17:23:40 -0500

    remove strict mode

--- src/index.js
+++ src/index.js
@@ -5,9 +5,7 @@ import reportWebVitals from './reportWebVitals';
 import './scss/app.scss';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App/>
-  </React.StrictMode>,
+  <App/>,
   document.getElementById('root'),
 );
 
